a217dd6ce62ad5b0414f6967fe47d7e761de7272
Thanushraj M M <thanushrajmm@gmail.com>
2024-08-19 15:21:02 +0530

    Create 09 Rising Temperature.sql

---
+++ 09 Rising Temperature.sql
@@ -0,0 +1,6 @@
+problem link -> [ https://leetcode.com/problems/rising-temperature ]
+SELECT W1.id
+FROM Weather W1
+JOIN Weather W2
+ON W1.recordDate = DATE_ADD(W2.recordDate, INTERVAL 1 DAY)
+WHERE W1.temperature > W2.temperature;
