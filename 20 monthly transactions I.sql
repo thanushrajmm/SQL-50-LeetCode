bb2c1761bfd6aea56b7feb25ab923c48180e6738
Thanushraj M M <thanushrajmm@gmail.com>
2024-11-03 20:58:06 +0530

    Create 20 monthly transactions I.sql

---
+++ 20 monthly transactions I.sql
@@ -0,0 +1,13 @@
+problem link -> [ https://leetcode.com/problems/monthly-transactions-i/ ] 
+# Write your MySQL query statement below
+SELECT 
+    LEFT(trans_date, 7) AS month,
+    country, 
+    COUNT(id) AS trans_count,
+    SUM(state = 'approved') AS approved_count,
+    SUM(amount) AS trans_total_amount,
+    SUM((state = 'approved') * amount) AS approved_total_amount
+FROM 
+    Transactions
+GROUP BY 
+    month, country;
