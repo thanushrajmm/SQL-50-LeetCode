447136cebe8f10af969da6cd55f38d8fe378221c
Thanushraj M M <thanushrajmm@gmail.com>
2024-11-12 08:57:49 +0530

    Create 27 Biggest Single Number.sql

---
+++ 27 Biggest Single Number.sql
@@ -0,0 +1,8 @@
+link -> [ https://leetcode.com/problems/biggest-single-number]
+SELECT MAX(num) AS num
+FROM (
+    SELECT num
+    FROM MyNumbers
+    GROUP BY num
+    HAVING COUNT(num) = 1
+) AS unique_numbers;
