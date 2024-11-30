5dfb01a0077c304685f45c0e7767f0af66898cdc
Thanushraj M M <thanushrajmm@gmail.com>
2024-11-30 12:56:55 +0530

    Create 29 The Number of Employees Which Report to Each Employee.sql

---
+++ 29 The Number of Employees Which Report to Each Employee.sql
@@ -0,0 +1,7 @@
+link -> [https://leetcode.com/problems/the-number-of-employees-which-report-to-each-employee/]
+
+select mgr.employee_id, mgr.name, COUNT(emp.employee_id) as reports_count, ROUND(AVG(emp.age)) as average_age
+from employees emp join employees mgr
+on emp.reports_to = mgr.employee_id
+group by employee_id
+order by employee_id
